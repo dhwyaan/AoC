4af3a5ca431d23ec10173df1c9ac7497c655f88e
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-07-24 18:58:41 +0545

    Update 2023Day2.cpp

--- 2023/2023Day2.cpp
+++ 2023/2023Day2.cpp
@@ -8,13 +8,14 @@
 bool colorChecker(std::string& colorname, int color){
     std::unordered_map<std::string, int> maxCubes = {{"blue", 14}, {"red", 12}, {"green", 13}};
     return maxCubes[colorname] >= color;
-    //return color <= maxCubes[colorname];
+    //return color <= maxCubes[colorname]; this is the same thing as above
 }
 
 int main(){
     std::ifstream input("input.txt");
     std::string line;
     int total = 0;
+    long power = 0;
     
     while (getline(input, line)){
         int colonPos = line.find(':');
@@ -29,14 +30,27 @@ int main(){
         std::replace(game.begin(), game.end(), ',', ' ');
         std::replace(game.begin(), game.end(), ';', ' ');
         std::stringstream ss = std::stringstream(game);
+        std::unordered_map<std::string, int> maxValues = {{"blue", 0}, {"red", 0}, {"green", 0}};
         
         while (ss >> color >> colorname){
             validity = validity && colorChecker(colorname, color);
+            if (maxValues[colorname] < color){
+                maxValues[colorname] = color;
+            }
         }
+        
         if (validity){
             total += gameNo;
         }
+        
+        int temp = 1;
+        for (auto it = maxValues.begin(); it != maxValues.end(); it++){
+            temp *= it->second;
+        }
+        
+        power += temp;
     }
     
-    std::cout << total;
+    std::cout << "Answer Part 1: "<< total << std::endl;
+    std::cout << "Answer Part 2: "<< power;
 }
