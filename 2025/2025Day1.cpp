858fab3e21895e4688809fd329966cc82125d06b
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-07-27 09:26:53 +0545

    Update 2025Day1.cpp

--- 2025/2025Day1.cpp
+++ 2025/2025Day1.cpp
@@ -31,3 +31,45 @@ int main()
     }
     std::cout << counter;
 }
+
+
+/* PART 2
+    #include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::ifstream input("input.txt");
+    std::string line;
+    int position = 50;
+    int counter = 0;
+    
+    while (getline(input, line)){
+        int change = std::stoi(line.substr(1));
+        char direction = line[0];
+        for (int i = 0; i < change; i++){
+            switch (direction){
+                case 'L':
+                position--;
+                break;
+                
+                case 'R':
+                position++;
+                break;
+            }
+            if (position<0){
+                position += 100;
+            }
+            else if (position>=100){
+                position -= 100;
+            }
+            if (position == 0){
+                counter++;
+            }
+        }
+    }
+    std::cout << counter;
+}
+
+*/
