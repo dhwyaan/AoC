1b05096eb5ff5769f9cf62fa84acb09d0feb6144
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-08-14 18:05:16 +0545

    Create 2015Day11.cpp

---
+++ 2015/2015Day11.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+bool first(const std::string& password){
+    int len = password.size();
+    for (int i = 0; i < len - 2; i++){
+        if (password[i + 1] == password[i] + 1  && password[i + 2] == password[i] + 2){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool second(const std::string& password){
+    if (password.find('i') != std::string::npos || password.find('o') != std::string::npos || password.find('l') != std::string::npos){
+        return false;
+    }
+    return true;
+}
+
+bool third(const std::string& password){
+    int count = 0;
+    int len = password.size();
+    for (int i = 0; i < len - 1; i++){
+        if (password[i] == password[i+1]){
+            count++;
+            i++;
+        }
+    }
+    return count >= 2;
+}
+
+void increment(std::string& password){
+    int len = password.size() - 1;
+    if (password[len] != 'z'){
+        password[len] += 1;
+    }
+    else{
+        while (password[len] == 'z'){
+            password[len] = 'a';
+            len--;
+            if (len == -1){
+                break;
+            }
+        }
+        password[len]++;
+    }
+}
+
+int main(){
+    std::string password = "cqjxjnds";
+    
+    while (!first(password) || !second(password) || !third(password)){
+        increment(password);
+    }
+    
+    std::cout << password;
+    
+    return 0;
+}
