229ccb0442906cfc0109f4f156c4cdc530e434da
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-08-14 20:13:15 +0545

    Update 2015Day12.cpp

--- 2015/2015Day12.cpp
+++ 2015/2015Day12.cpp
@@ -3,6 +3,25 @@
 #include <sstream>
 #include <string>
 
+int linesum (std::string line){
+    int len = line.size();
+    int result = 0;
+    for (int i = 0; i < len; i++){
+        if ((line[i] >= '0' && line[i] <= '9') || line[i] == '-'){
+            continue;
+        }
+        else{
+            line[i] = ' ';
+        }
+    }
+    std::stringstream ss(line);
+    std::string num;
+    while(ss >> num){
+        result += std::stoi(num);
+    }
+    return result;
+}
+
 int main()
 {
     std::ifstream input("input.txt");
@@ -10,20 +29,7 @@ int main()
     int result = 0;
     
     while(getline(input, line)){
-        int len = line.size();
-        for (int i = 0; i < len; i++){
-            if ((line[i] >= '0' && line[i] <= '9') || line[i] == '-'){
-                continue;
-            }
-            else{
-                line[i] = ' ';
-            }
-        }
-        std::stringstream ss(line);
-        std::string num;
-        while(ss >> num){
-            result += std::stoi(num);
-        }
+        result += linesum(line);
     }
     std::cout << result;
 }
